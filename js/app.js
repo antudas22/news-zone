d2aa0fbf682e87a0093585a930dbc11c095c7b8e
antudas22 <20antu20@gmail.com>
2022-09-04 01:01:40 +0600

    category items image, title, paragraph added

--- js/app.js
+++ js/app.js
@@ -1,20 +1,52 @@
-const loadCategories = async() =>{
+const loadCategoriesName = async() =>{
     const url = `https://openapi.programming-hero.com/api/news/categories`
     const res = await fetch(url);
     const data = await res.json();
-    displayCategories(data.data.news_category);
+    displayCategoriesName(data.data.news_category);
 }
 
-const displayCategories = categories =>{
+const displayCategoriesName = categories =>{
     const categorySection = document.getElementById('category-section');
     categories.forEach(category =>{
         const categoryDiv = document.createElement('div');
         categoryDiv.classList.add('fs-4', 'mt-2');
         categoryDiv.innerHTML = `
-        <button class="bg-white border-0">${category.category_name}</button>
+        <button onclick="loadData(${category.category_id})" class="bg-white border-0">${category.category_name}</button>
         `;
         categorySection.appendChild(categoryDiv);
     })
 }
 
-loadCategories()
+const loadData = (id) =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayData(data.data));
+}
+
+const displayData = items =>{
+    const showItems = document.getElementById('show-items');
+    items.forEach(item =>{
+        const itemDiv = document.createElement('div');
+        itemDiv.innerHTML = `
+        <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-md-4">
+            <img src="${item.image_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">${item.title}</h5>
+                <p class="card-text">${item.details.slice(0, 280)}...</p>
+            </div>
+            </div>
+        </div>
+        </div>
+        `;
+        showItems.appendChild(itemDiv);
+    })
+}
+
+loadData();
+
+loadCategoriesName();
