c37ea0a494ad08f7f00467e493ff2415b8dbf5cc
antudas22 <20antu20@gmail.com>
2022-09-04 05:27:14 +0600

    modal section added

--- js/app.js
+++ js/app.js
@@ -17,31 +17,44 @@ const displayCategoriesName = categories =>{
     })
 }
 
-const loadData = (id) =>{
+const loadData = async(id) =>{
     const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayData(data.data));
-    // .then(data => console.log(data.data));
+    const res = await fetch(url)
+    const data = await res.json();
+    displayData(data.data);
 }
 
 const displayData = items =>{
+    
     const showItems = document.getElementById('show-items');
+    // showItems.textContent = '';
     items.forEach(item =>{
+        const modalTitle = document.getElementById('exampleModalLabel');
+        modalTitle.innerText = item.title;
+        const modalDetails = document.getElementById('modal-details');
+        modalDetails.innerHTML = `
+        <img src="${item.image_url}" class="img-fluid rounded-start" alt="...">
+        <p class="card-text">${item.details}</p>
+        `;
     const foundItems = document.getElementById('items-found');
     foundItems.innerText = `${items.length} Items Found
         `;
         const itemDiv = document.createElement('div');
         itemDiv.innerHTML = `
-        <div class="card mb-3">
+        <div class="card mb-3 border-0 shadow p-3 mb-5 bg-body rounded">
         <div class="row g-0">
             <div class="col-md-4">
-            <img src="${item.image_url}" class="img-fluid rounded-start" alt="...">
+            <img src="${item.thumbnail_url}" class="img-fluid rounded-start" alt="...">
             </div>
             <div class="col-md-8">
             <div class="card-body">
                 <h5 class="card-title">${item.title}</h5>
                 <p class="card-text">${item.details.slice(0, 280)}...</p>
+            <div class="d-flex justify-content-between align-items-center mt-5">
+                <div class="d-flex gap-2 align-items-center"><img src="${item.author.img}" style="width: 50px;" class="rounded-circle"> <h4>${item.author.name ? item.author.name : 'No Data'}</h4></div>
+                <div class=""><p>Views: ${item.total_view ? item.total_view : 'No Data'}</p></div>
+                <div><button id="btn-explore" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Explore</button></div>
+            </div>
             </div>
             </div>
         </div>
@@ -50,6 +63,8 @@ const displayData = items =>{
         showItems.appendChild(itemDiv);
     })
 }
+
+
 
 loadData('8');
 
